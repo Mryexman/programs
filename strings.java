4f2b63ee057b4151b7ec6b3a10340cfdbdd2b38a
mr YEX <100796968+Mryexman@users.noreply.github.com>
2023-07-17 21:40:14 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,18 @@
+// string example
+public class string {
+public static void main(String args[])
+{
+String s1="LEELA";
+String s2="madhuri";
+
+
+System.out.println(s1.length());
+System.out.println(s1.equals(s2));
+System.out.println(s1.toLowerCase());
+System.out.println(s1.toUpperCase());
+System.out.println(s1.compareTo(s2));
+System.out.println(s2.replace("i","e"));
+System.out.println(s2.charAt(4));
+System.out.println(s1.concat(s2));
+}
+}
