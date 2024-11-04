c8f0900236fe6831b4131f1f93579ed824cdcffa
mr YEX <100796968+Mryexman@users.noreply.github.com>
2024-11-04 20:41:37 +0530

    Update

---
+++ series.py
@@ -0,0 +1,26 @@
+Write a program to generate the Fibonacci sequence
+
+Program :
+n=int(input("Enter n :"))
+a=0
+b=1
+print("Fibonacci series : ")
+print(a)
+print(b)
+i=2
+while i<n:
+c=a+b
+print(c)
+a=b
+b=c
+i+=1
+
+Output :
+Enter n :6
+Fibonacci series :
+0
+1
+1
+2
+3
+5
