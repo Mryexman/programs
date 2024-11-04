41cdae73d8efae15a37ea6f5593deb86c11fdd89
mr YEX <100796968+Mryexman@users.noreply.github.com>
2024-11-04 20:40:47 +0530

    Update

---
+++ armstrong.py
@@ -0,0 +1,19 @@
+Write a program to check whether the given number is Armstrong or not
+
+Program
+
+a=int(input("Enter a number :"))
+sum=0
+temp=a
+while a>0:
+b=a%10
+sum+=(b**3)
+a=a//10
+if(sum==temp):
+print(temp,"is an Armstrong number")
+else:
+print(temp,"is not an Armstrong number ")
+
+Output :
+Enter a number :370
+370 is an Armstrong number
