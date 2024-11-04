9d77cc3766902e395e0c9f85ff4b90008f354067
mr YEX <100796968+Mryexman@users.noreply.github.com>
2024-11-04 20:30:11 +0530

    Update

---
+++ swap.py
@@ -0,0 +1,12 @@
+Swap two numbers ?
+Program:
+x = 5
+y = 7
+print ("Before swapping: ")
+print("Value of x : ", x, " and y: ", y)# code to swap 'x' and 'y'x, y = y,x
+print ("After swapping: ")
+print("Value of x: ", x, " and y: ", y)
+
+Output:
+Before swapping:Value of x: 5 andy: 7
+After swapping:Value of x: 7 andy: 5
