57d7f02339d51b5d93083b38ce69fc4e916c82f3
mr YEX <100796968+Mryexman@users.noreply.github.com>
2024-11-04 20:33:33 +0530

    Update

---
+++ expo.py
@@ -0,0 +1,17 @@
+Exponentiation (power of a number)
+
+Program:
+
+def CalculatePower(N,X):
+P=1
+for i in range(1, X+1)
+P=P*N
+return P
+N,X=2,3
+print(CalculatePower(N,X))
+N,X=3,4
+print(CalculatePower(N,X))
+
+Output:
+8
+81
