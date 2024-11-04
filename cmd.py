4e226e70ac089b0fd621711d500643fd8c36bec1
mr YEX <100796968+Mryexman@users.noreply.github.com>
2024-11-04 20:39:57 +0530

    Update

---
+++ cmd.py
@@ -0,0 +1,26 @@
+Programs that take command line arguments(word count)
+
+Program:
+
+import sys
+# total arguments
+n = len(sys.argv)
+print("Total arguments passed:", n)
+# Arguments passed
+print("\nName of Python script:", sys.argv[0])
+print("\nArguments passed:", end = " ")
+for i in range(1, n):
+print(sys.argv[i], end = " ")
+#Addition of numbers
+Sum = 0
+# Using argparse module
+for i in range(1, n):
+Sum += int(sys.argv[i])
+print("\n\nResult:", Sum)
+
+output :
+ python cmd.py 1 2 3 4 5 6
+total args passed : 7
+name of py script : cmd.py
+args passed: 1 2 3 4 5 6
+result : 21
